aed52646ebcbe2d7de1fbb0e81196273515a106b
Az Vasquez <azvasquez99@gmail.com>
2021-07-01 23:55:26 -0400

    Prestash boorus

--- server.js
+++ server.js
@@ -5,22 +5,28 @@ const port = 8000;
 const Stream = require('stream').Transform;
 const https = require('https');
 const Danbooru = require('danbooru');
-const booru = new Danbooru()
+const booru = new Danbooru();
 
-app.get('/img', (req,res) => {
-    booru.posts({ tags: 'rating:safe order:rank' }).then(posts => {
+let booruCache = [];
 
-      const index = Math.floor(Math.random() * posts.length)
-      const post = posts[index]
+function cacheBoorus() {
+    booru.posts({ tags: 'rating:safe order:rank', limit: 100 }).then(posts => {
+        for (let post of posts) {
+            const url = booru.url(post.file_url)
 
-      const url = booru.url(post.file_url)
+            https.get(url, imgResponse => {
+                var data = new Stream();
+                imgResponse.on('data', chunk => data.push(chunk));
+                imgResponse.on('end', () => booruCache.push(data.read()));
+            });
+        }
 
-      https.get(url, imgResponse => {
-        var data = new Stream();
-        imgResponse.on('data', chunk => data.push(chunk));
-        imgResponse.on('end', () => res.end(data.read()));
-      });
     });
+}
+cacheBoorus();
+
+app.get('/img', (req, res) => {
+    res.send(booruCache.shift());
 });
 
 app.use(express.static('public'))
