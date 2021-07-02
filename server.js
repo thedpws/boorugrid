f8fbf68db028515e1343825b47071f541f312745
Az Vasquez <azvasquez99@gmail.com>
2021-07-01 23:11:43 -0400

    Download beautiful images at /img

---
+++ server.js
@@ -0,0 +1,34 @@
+
+const express = require('express');
+const app = express();
+const port = 8000;
+const Stream = require('stream').Transform;
+const https = require('https');
+const Danbooru = require('danbooru');
+const booru = new Danbooru()
+
+app.get('/img', (req,res) => {
+    booru.posts({ tags: 'rating:safe order:rank' }).then(posts => {
+
+      const index = Math.floor(Math.random() * posts.length)
+      const post = posts[index]
+
+      const url = booru.url(post.file_url)
+
+      https.get(url, imgResponse => {
+        var data = new Stream();
+
+        imgResponse.on('data', function(chunk) {
+          data.push(chunk);
+        });
+
+        imgResponse.on('end', function() {
+            res.end(data.read());
+        });
+      });
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+});
